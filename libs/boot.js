30be3ae27c2081513b2d33d35a05a95a402554ef
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-09 16:28:07 +0100

    Refactor libs/boot module

--- libs/boot.js
+++ libs/boot.js
@@ -1,13 +1,13 @@
-export default function (api) {
+module.exports = (api) => {
   let port;
   const env = process.env.ENV || 'dev';
 
-  if (process.env.ENV !== 'test') {
-    port = process.env.PORT || 5000;
-  } else {
+  if (process.env.ENV === 'test') {
     port = process.env.PORT_TEST || 5005;
+  } else {
+    port = process.env.PORT || 5000;
   }
   api.listen(port, () => {
     console.log(`[${env}] API has started listening at port:${port}`);
   });
-}
+};
