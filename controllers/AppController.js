042528c8c1caecad4d1be8fd77e885943b848e5e
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-09 16:37:21 +0100

    Add App controller

---
+++ controllers/AppController.js
@@ -0,0 +1,18 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
+  }
+}
