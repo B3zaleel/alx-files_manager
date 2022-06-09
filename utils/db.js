6f986fb5e6abf46a02282223b9cec3791ad8d090
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-09 15:26:23 +0100

    :sparkles: Create MongoDB client wrapper

---
+++ utils/db.js
@@ -0,0 +1,46 @@
+import mongodb from 'mongodb';
+
+/**
+ * Represents a MongoDB client.
+ */
+class DBClient {
+  /**
+   * Creates a new DBClient instance.
+   */
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  /**
+   * Checks if this client's connection to the MongoDB server is active.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  /**
+   * Retrieves the number of users in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  /**
+   * Retrieves the number of files in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
