e8c7be5f2785d7a94aafc1fdc94f186d16000c01
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-09 16:53:29 +0100

    Refactor routes/index module

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import { Express } from 'express';
 import AppController from '../controllers/AppController';
 
-module.exports = (api) => {
+/**
+ * Injects routes with their handlers to the given Express application.
+ * @param {Express} api
+ */
+const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
 };
+
+module.exports = injectRoutes;
