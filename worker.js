a7b5a4816813a07704c46bba6df6fcbae770a582
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-10 11:28:27 +0100

    Create worker module for generating thumbnails

---
+++ worker.js
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-named-as-default */
+import { writeFile } from 'fs';
+import { promisify } from 'util';
+import Queue from 'bull/lib/queue';
+import imgThumbnail from 'image-thumbnail';
+import mongoDBCore from 'mongodb/lib/core';
+import dbClient from './utils/db';
+
+const writeFileAsync = promisify(writeFile);
+const fileQueue = new Queue('thumbnail generation');
+
+/**
+ * Generates the thumbnail of an image with a given width size.
+ * @param {String} filePath The location of the original file.
+ * @param {number} size The width of the thumbnail.
+ * @returns {Promise<void>}
+ */
+const generateThumbnail = async (filePath, size) => {
+  const buffer = await imgThumbnail(filePath, { width: size });
+  return writeFileAsync(`${filePath}_${size}`, buffer);
+};
+
+fileQueue.process(async (job, done) => {
+  const fileId = job.data.fileId || null;
+  const userId = job.data.userId || null;
+
+  if (!fileId) {
+    throw new Error('Missing fileId');
+  }
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+  const file = await (await dbClient.filesCollection())
+    .findOne({
+      _id: new mongoDBCore.BSON.ObjectId(fileId),
+      userId,
+    });
+  if (!file) {
+    throw new Error('File not found');
+  }
+  const sizes = [500, 250, 100];
+  Promise.all(sizes.map((size) => generateThumbnail(file.localPath, size)))
+    .then(() => {
+      done();
+    });
+});
