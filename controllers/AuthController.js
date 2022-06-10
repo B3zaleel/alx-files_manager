eaae08a2f634d8b95a73444e5801374c8329de36
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-10 07:22:13 +0100

    Refactor controllers/AuthController.js module

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,29 +1,19 @@
 /* eslint-disable import/no-named-as-default */
-import { v4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
-import { getUserFromAuthorization, getUserFromXToken } from '../utils/auth';
 
 export default class AuthController {
   static async getConnect(req, res) {
-    const user = await getUserFromAuthorization(req);
+    const { user } = req;
+    const token = uuidv4();
 
-    if (!user) {
-      res.status(401).json({ error: 'Unauthorized' });
-      return;
-    }
-    const token = v4();
     await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
     res.status(200).json({ token });
   }
 
   static async getDisconnect(req, res) {
-    const user = await getUserFromXToken(req);
-
-    if (!user) {
-      res.status(401).json({ error: 'Unauthorized' });
-      return;
-    }
     const token = req.headers['X-Token'];
+
     await redisClient.del(`auth_${token}`);
     res.status(204).json({});
   }
