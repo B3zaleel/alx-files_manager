8c68e383a93a441ec95ba9f12d2a4dfb39709ca8
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-10 08:40:16 +0100

    Implement FilesController.putUnpublish method

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -138,8 +138,30 @@ export default class FilesController {
     });
   }
 
-  static putUnpublish(req, res) {
-    res.status(200).json({});
+  static async putUnpublish(req, res) {
+    const { user } = req;
+    const { id } = req.params;
+    const userId = user._id.toString();
+    const fileFilter = {
+      _id: new mongoDBCore.BSON.ObjectId(id),
+      userId,
+    };
+    const file = await (await dbClient.filesCollection())
+      .findOne(fileFilter);
+
+    if (!file) {
+      throw new APIError(404, 'Not found');
+    }
+    await (await dbClient.filesCollection())
+      .updateOne(fileFilter, { isPublic: false });
+    res.status(200).json({
+      id,
+      userId,
+      name: file.name,
+      type: file.type,
+      isPublic: false,
+      parentId: file.parentId,
+    });
   }
 
   static getFile(req, res) {
