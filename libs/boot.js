1bfa1b25a5f66551da20a8a373723fd24ccb2222
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-11 07:24:08 +0100

    Refactor startup script

--- libs/boot.js
+++ libs/boot.js
@@ -1,12 +1,10 @@
-const startServer = (api) => {
-  let port;
-  const env = process.env.ENV || 'dev';
+import { config } from 'dotenv';
 
-  if (process.env.ENV === 'test') {
-    port = process.env.PORT_TEST || 5005;
-  } else {
-    port = process.env.PORT || 5000;
-  }
+const startServer = (api) => {
+  const port = process.env.PORT || 5000;
+  const env = process.env.npm_lifecycle_event || 'dev';
+
+  config({ path: env.includes('test') ? '.env.test' : '.env' });
   api.listen(port, () => {
     console.log(`[${env}] API has started listening at port:${port}`);
   });
