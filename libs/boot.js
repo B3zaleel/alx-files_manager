aeed8812cecad9312d691076a5f285ab785a63ea
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-11 09:56:32 +0100

    Use NodeRequire for loading .env files

--- libs/boot.js
+++ libs/boot.js
@@ -1,10 +1,13 @@
-import { config } from 'dotenv';
+import { existsSync } from 'fs';
 
 const startServer = (api) => {
-  const port = process.env.PORT || 5000;
   const env = process.env.npm_lifecycle_event || 'dev';
+  if (existsSync('.env') || existsSync('.env.test')) {
+    const dotenv = require('dotenv');
+    dotenv.config({ path: env.includes('test') ? '.env.test' : '.env' });
+  }
+  const port = process.env.PORT || 5000;
 
-  config({ path: env.includes('test') ? '.env.test' : '.env' });
   api.listen(port, () => {
     console.log(`[${env}] API has started listening at port:${port}`);
   });
