d11a096a916843b926adbb2e8ff02beebc2f8cad
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-11 05:54:19 +0100

    Revert "Remove middleware for AuthController route handlers"
    This reverts commit 52a5530b33e1e207eec5dd0c80722aa2b9a2c273.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,16 +1,10 @@
 /* eslint-disable import/no-named-as-default */
 import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
-import { getUserFromXToken, getUserFromAuthorization } from '../utils/auth';
 
 export default class AuthController {
   static async getConnect(req, res) {
-    const user = await getUserFromAuthorization(req);
-
-    if (!user) {
-      res.status(401).json({ error: 'Unauthorized' });
-      return;
-    }
+    const { user } = req;
     const token = uuidv4();
 
     await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
@@ -18,13 +12,8 @@ export default class AuthController {
   }
 
   static async getDisconnect(req, res) {
-    const user = await getUserFromXToken(req);
     const token = req.headers['X-Token'];
 
-    if (!user) {
-      res.status(401).json({ error: 'Unauthorized' });
-      return;
-    }
     await redisClient.del(`auth_${token}`);
     res.status(204).send();
   }
