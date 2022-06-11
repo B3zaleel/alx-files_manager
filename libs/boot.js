ece3db659afd2f2db2ac54b9bc1fda6fbc8c9a8d
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-11 09:58:25 +0100

    Revert "Use NodeRequire for loading .env files"
    This reverts commit aeed8812cecad9312d691076a5f285ab785a63ea.

--- libs/boot.js
+++ libs/boot.js
@@ -1,13 +1,10 @@
-import { existsSync } from 'fs';
+import { config } from 'dotenv';
 
 const startServer = (api) => {
-  const env = process.env.npm_lifecycle_event || 'dev';
-  if (existsSync('.env') || existsSync('.env.test')) {
-    const dotenv = require('dotenv');
-    dotenv.config({ path: env.includes('test') ? '.env.test' : '.env' });
-  }
   const port = process.env.PORT || 5000;
+  const env = process.env.npm_lifecycle_event || 'dev';
 
+  config({ path: env.includes('test') ? '.env.test' : '.env' });
   api.listen(port, () => {
     console.log(`[${env}] API has started listening at port:${port}`);
   });
