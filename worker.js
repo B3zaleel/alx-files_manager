f382fea402c5101eede14008684a4db60cc4303c
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-11 02:44:13 +0100

    Integrate mail sending module in **email sending** queue processor

--- worker.js
+++ worker.js
@@ -5,6 +5,7 @@ import Queue from 'bull/lib/queue';
 import imgThumbnail from 'image-thumbnail';
 import mongoDBCore from 'mongodb/lib/core';
 import dbClient from './utils/db';
+import Mailer from './utils/mailer';
 
 const writeFileAsync = promisify(writeFile);
 const fileQueue = new Queue('thumbnail generation');
@@ -58,6 +59,21 @@ userQueue.process(async (job, done) => {
     throw new Error('User not found');
   }
   console.log(`Welcome ${user.email}!`);
-  // TODO: Send an actual email
-  done();
+  try {
+    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
+    const mailContent = [
+      '<div>',
+      '<h3>Hello {{user.name}},</h3>',
+      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
+      'ALX-Files_Manager</a>, ',
+      'a simple file management API built with Node.js by ',
+      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
+      'We hope it meets your needs.',
+      '</div>',
+    ].join('');
+    Mailer.sendMail(Mailer.buildMessage(user.email, mailSubject, mailContent));
+    done();
+  } catch (err) {
+    done(err);
+  }
 });
