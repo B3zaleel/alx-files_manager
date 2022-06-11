ac6e8e5c35ed7f57c72cbb58d4ae8be6b9ae65dc
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-11 10:23:27 +0100

    Revert "Comment out environment loading routine"
    This reverts commit 7bf262be0e16c9d5b0dba9f06c88eaf3c76162a4.

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 import mongodb from 'mongodb';
 // eslint-disable-next-line no-unused-vars
 import Collection from 'mongodb/lib/collection';
-// import envLoader from './env_loader';
+import envLoader from './env_loader';
 
 /**
  * Represents a MongoDB client.
@@ -11,7 +11,7 @@ class DBClient {
    * Creates a new DBClient instance.
    */
   constructor() {
-    // envLoader();
+    envLoader();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
