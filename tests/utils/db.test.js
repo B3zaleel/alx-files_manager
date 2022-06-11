f8ad85664861535801f1d1903bdd9e4007c69eb7
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-11 07:49:58 +0100

    Add tests for dbClient

---
+++ tests/utils/db.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-named-as-default */
+import dbClient from '../../utils/db';
+
+describe('dbClient utility', () => {
+  before(function (done) {
+    this.timeout(10000);
+    Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
+      .then(([usersCollection, filesCollection]) => {
+        Promise.all([usersCollection.deleteMany({}), filesCollection.deleteMany({})])
+          .then(() => done())
+          .catch((deleteErr) => done(deleteErr));
+      }).catch((connectErr) => done(connectErr));
+    setTimeout(done, 5000);
+  });
+
+  it('is alive', () => {
+    expect(dbClient.isAlive()).to.equal(true);
+  });
+
+  it('nbUsers function', async () => {
+    expect(await dbClient.nbUsers()).to.equal(0);
+  });
+
+  it('nbFiles function', async () => {
+    expect(await dbClient.nbFiles()).to.equal(0);
+  });
+});
