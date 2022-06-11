e3c4cd92663bcabe70cc0ba7fb2dc7902f84b21d
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-11 13:53:10 +0100

    Add tests for GET: /connect

---
+++ tests/controllers/AuthController.test.js
@@ -0,0 +1,82 @@
+/* eslint-disable import/no-named-as-default */
+import dbClient from '../../utils/db';
+
+describe('+ AuthController', () => {
+  const mockUser = {
+    email: 'kaido@beast.com',
+    password: 'hyakuju_no_kaido_wano',
+  };
+  let token = '';
+
+  before(function (done) {
+    this.timeout(10000);
+    dbClient.usersCollection()
+      .then((usersCollection) => {
+        usersCollection.deleteMany({ email: mockUser.email })
+          .then(() => {
+            request.post('/users')
+              .send({
+                email: mockUser.email,
+                password: mockUser.password,
+              })
+              .expect(201)
+              .end((requestErr, res) => {
+                if (requestErr) {
+                  return done(requestErr);
+                }
+                expect(res.body.email).to.eql(mockUser.email);
+                expect(res.body.id.length).to.be.greaterThan(0);
+                done();
+              });
+          })
+          .catch((deleteErr) => done(deleteErr));
+      }).catch((connectErr) => done(connectErr));
+    setTimeout(done, 5000);
+  });
+
+  describe('+ GET: /connect', () => {
+    it('+ Fails with no "Authorization" header field', function (done) {
+      this.timeout(5000);
+      request.get('/connect')
+        .expect(401)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.eql({ error: 'Unauthorized' });
+          done();
+        });
+    });
+
+    it('+ Fails for a non-existent user', function (done) {
+      this.timeout(5000);
+      request.get('/connect')
+        .auth('foo@bar.com', 'raboof', { type: 'basic' })
+        .expect(401)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.eql({ error: 'Unauthorized' });
+          done();
+        });
+    });
+
+    it('+ Succeeds for an existing user', function (done) {
+      this.timeout(5000);
+      request.get('/connect')
+        .auth(mockUser.email, mockUser.password, { type: 'basic' })
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body.token).to.exist;
+          expect(res.body.token.length).to.be.greaterThan(0);
+          token = res.body.token;
+          done();
+        });
+    });
+  });
+
+});
