db375cc81aaf2d5621eddf0db9372f0a14853e41
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-12 04:23:32 +0100

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
-    jest: true,
+    jest: false,
+    mocha: true,
   },
   extends: [
     'airbnb-base',
@@ -11,6 +12,7 @@ module.exports = {
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    request: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
@@ -28,7 +30,7 @@ module.exports = {
       'WithStatement',
     ],
   },
-  overrides:[
+  overrides: [
     {
       files: ['*.js'],
       excludedFiles: 'babel.config.js',
