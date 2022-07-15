d79ee4082666b714bf2a288f22e9ef2318057ea0
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-07-15 06:12:03 +0100

    Revert "Change response value of FilesController.getShow"
    This reverts commit 84878621a9bc07cb7432fb5ac80d9ca7e2b6ea12.

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -147,7 +147,16 @@ export default class FilesController {
       res.status(404).json({ error: 'Not found' });
       return;
     }
-    res.status(200).json(file);
+    res.status(200).json({
+      id,
+      userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId === ROOT_FOLDER_ID.toString()
+        ? 0
+        : file.parentId.toString(),
+    });
   }
 
   /**
